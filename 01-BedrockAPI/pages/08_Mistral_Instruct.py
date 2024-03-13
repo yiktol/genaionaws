04bbf0efc47c6e5d4afa4cca4a88c9faa28cbb1f
Eric <erictole@amazon.com>
2024-03-14 07:28:03 +0800

    Updates

--- 01-BedrockAPI/pages/08_Mistral_Instruct.py
+++ 01-BedrockAPI/pages/08_Mistral_Instruct.py
@@ -7,7 +7,7 @@ set_page_config()
 
 helpers.reset_session()
 
-bedrock_runtime = bedrock_runtime_client(region='us-west-2')
+bedrock_runtime = bedrock_runtime_client(region='us-east-1')
 
 dataset = helpers.load_jsonl('data/mistral.jsonl')
 
@@ -21,7 +21,6 @@ with text:
 - A 7B dense Transformer, fast-deployed and easily customisable. Small, yet powerful for a variety of use cases. Supports English and code, and a 32k context window.
 - A 7B sparse Mixture-of-Experts model with stronger capabilities than Mistral 7B. Uses 12B active parameters out of 45B total. Supports multiple languages, code and 32k context window.
                 """)
-
 
     with st.expander("See Code"):
         st.code(helpers.render_mistral_code('instruct.jinja'),language="python")
@@ -59,7 +58,7 @@ with text:
             st.info(f"{response_body.get('outputs')[0].get('text')}")
 
 with code:
-    helpers.tune_parameters('Mistral', index=0,region='us-west-2')
+    helpers.tune_parameters('Mistral', index=0,region='us-east-1')
     st.subheader('Prompt Examples:')   
     container2 = st.container(border=True) 
     with container2:
